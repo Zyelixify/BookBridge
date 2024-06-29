8fbdbd807e83378d94b23bc1af787c135076fcac
Zyelixify <49106310+Zyelixify@users.noreply.github.com>
2024-06-29 13:35:52 +0200

    updated prisma schema and route shielding

--- server/trpc/procedures.ts
+++ server/trpc/procedures.ts
@@ -1,22 +1,8 @@
-import { allow, and, deny, or, rule, shield } from 'trpc-shield'
+import { rule, shield } from 'trpc-shield'
 import { publicProcedure, t } from './trpc'
 import type { Context } from '~/server/trpc/context'
 
 const isAuth = rule<Context>()(ctx => !!ctx.session?.user)
-const permissions = shield<Context>({
-  query: {
-    // Account
-    findOneAccount: isAuth,
-    findManyAccount: isAuth,
-
-    // Book
-    findOneBook: isAuth,
-    findManyBook: isAuth,
-  },
-  mutation: {
-    createBook: isAuth,
-    updateBook: isAuth,
-  },
-}, { fallbackRule: deny })
+const permissions = shield<Context>({}, { fallbackRule: isAuth })
 
 export const shieldedProcedure = publicProcedure.use(t.middleware(permissions))
