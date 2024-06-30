27679f823ed11b7dbbbfe566d65b5c4a813b6a62
Zyelixify <49106310+Zyelixify@users.noreply.github.com>
2024-06-30 14:10:55 +0200

    updated book loading

--- server/trpc/routers/book.ts
+++ server/trpc/routers/book.ts
@@ -50,20 +50,21 @@ export const router = createRouter({
 
 async function loadBooks(ctx: Context) {
   const existingCount = await ctx.prisma.book.count()
-  if (existingCount > 0) { return }
+  if (existingCount > 0) {
+    return
+  }
 
   const response = await fetch('http://openlibrary.org/subjects/fiction.json?limit=51')
   const data = await response.json()
   const booksData = data.works
 
   const formattedBooks = booksData.map((book: any) => {
-    // Attempt to find an ISBN 13 or ISBN 10 from the book's identifiers
-    const isbn13 = book.identifiers?.isbn_13?.[0] // Taking the first ISBN 13 if available
-    const isbn10 = book.identifiers?.isbn_10?.[0] // Taking the first ISBN 10 if available
-    const isbn = isbn13 || isbn10 // Prefer ISBN 13 over ISBN 10
+    const isbn13 = book.identifiers?.isbn_13?.[0]
+    const isbn10 = book.identifiers?.isbn_10?.[0]
+    const isbn = isbn13 || isbn10
 
     return {
-      isbn: isbn || `N/A`, // Use 'N/A' if no ISBN is available
+      isbn: isbn || `N/A`,
       title: book.title,
       author: book.authors[0]?.name || 'Unknown Author',
       tags: {
@@ -73,7 +74,7 @@ async function loadBooks(ctx: Context) {
         })),
       },
       image: book.cover_id ? `http://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg` : null,
-      price: Math.random() * 100, // Generating a random price.
+      price: Math.random() * 100,
       publishedAt: new Date(`${book.first_publish_year}-01-01`),
       createdAt: new Date(),
       status: 'available',
@@ -81,5 +82,24 @@ async function loadBooks(ctx: Context) {
     }
   })
 
-  ctx.prisma.$transaction(formattedBooks.map((book: any) => ctx.prisma.book.create({ data: book })))
+  // Utility function to chunk the array
+  const chunkSize = 10 // Adjust based on your connection pool size and load
+  const chunks = chunkArray(formattedBooks, chunkSize)
+
+  for (const chunk of chunks) {
+    await ctx.prisma.$transaction(
+      chunk.map((book: any) => ctx.prisma.book.create({ data: book }))
+    )
+  }
+}
+
+// Utility function to chunk an array into smaller arrays of a specified size
+function chunkArray(array: any[], size: number): any[][] {
+  return array.reduce((acc, val, i) => {
+    const idx = Math.floor(i / size)
+    const page = acc[idx] || (acc[idx] = [])
+    page.push(val)
+
+    return acc
+  }, [])
 }
